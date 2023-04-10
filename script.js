884b88a572a88de7caa1a566936fb8000cc67f6b
Andreyanna <andreyanna.it@gmail.com>
2023-04-10 12:36:31 +0800

    выполнила задание

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const userName = document.querySelector("#name");
+const secondName = document.querySelector("#secondName");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const agree = document.querySelector("#agree");
 
-  // Здесь твой код
+form.addEventListener("submit", (event) => {
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
+    fetch('https://polinashneider.space/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: Andreyanna'
+            },
+            body: JSON.stringify({
+                name: `${userName.value}`,
+                secondName: `${secondName.value}`,
+                email: `${email.value}`,
+                phone: `${phone.value}`,
+                agree: `${agree.checked}`
+            })
+        })
+        .then((result) => {
+            return result.json()
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch(() => {
+            form.reset();
+            alert('Ошибка! Попробуйте еще раз.');
+        })
+        .finally(() => {
+            form.reset();
+            alert('Данные отправлены!');
+        })
 });
